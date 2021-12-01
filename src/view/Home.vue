98a8bb5154ed8b5bf21fbfcdea931530bd09ecfb
ErwinFeld <erwinmailkonto@gmail.com>
2021-12-01 19:59:33 +0100

    added test component

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <nav-bar />
   <calc-option/>
+  <computed-test/>
   <!-- <calculation-section/> -->
 </template>
 
@@ -8,13 +9,15 @@
 import NavBar from "../components/NavBar.vue";
 // import CalculationSection from '../components/CalculationSection.vue'
 import CalcOption from '../components/CalcOption.vue'
+import ComputedTest from '../components/ComputedTest.vue';
 
 
 export default {
   name: "Home",
   components: {
     NavBar,
-    CalcOption
+    CalcOption,
+    ComputedTest
     // CalculationSection,
   },
 };
