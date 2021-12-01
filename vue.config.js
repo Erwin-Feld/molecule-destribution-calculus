da27095995d32d3ed391a95aed27e1e48969cacd
ErwinFeld <erwinmailkonto@gmail.com>
2021-12-01 19:58:43 +0100

    added debuuging map dont work though

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+// vue.config.js
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+ module.exports = {
+    configureWebpack: {
+        devtool: 'source-map'
+    }
+}
+
+
 // module.exports = {
 //   lintOnSave: false,
 // };
